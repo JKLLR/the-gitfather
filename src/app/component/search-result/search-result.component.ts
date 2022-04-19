303b5dcf7e8d60168295abf0dc0f0178143a515a
JKLLR <jeffhuria@gmail.com>
2022-04-19 03:54:24 +0300

    add changes to search-result typescript

--- src/app/component/search-result/search-result.component.ts
+++ src/app/component/search-result/search-result.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersClass } from 'src/app/users-class';
+import { SearchGitService } from 'src/app/service/search-git.service';
+import { RepoClass } from 'src/app/repo-class';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RepoByName } from 'src/app/repo-by-name';
 
 @Component({
   selector: 'app-search-result',
@@ -6,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-result.component.css']
 })
 export class SearchResultComponent implements OnInit {
+  
+  repos:RepoClass[];
+  user:UsersClass;
+  username:string; 
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private searchGitService: SearchGitService) { }
+
+  searchResult(){
+    this.username = this.route.snapshot.paramMap.get('username')
+    this.searchGitService.getUserData(this.username)
+    this.user = this.searchGitService.user
+    this.searchGitService.getRepositoryData(this.username)
+    this.repos =this.searchGitService.repos
+  }
 
   ngOnInit(): void {
+    this.searchResult()
   }
 
 }
